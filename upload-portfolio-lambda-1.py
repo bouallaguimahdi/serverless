09cca76b93184f739c0fc98cf557dab8b26d0a9e
Mehdi B <bouallagui.mahdi@gmail.com>
2019-02-07 18:46:32 +0100

    adjust the SNS

--- upload-portfolio-lambda-1.py
+++ upload-portfolio-lambda-1.py
@@ -9,6 +9,8 @@ def lambda_handler(event, context):
     portfolio_bucket = s3.Bucket('portfolio.bouallag.rrdog.myinstance.com')
     build_bucket = s3.Bucket('portfoliobuild.bouallag')
     build_bucket.download_file('portfoliobuild.zip', '/tmp/portfoliobuild.zip')
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:390072403864:portfolio-topic')
 
 
     with zipfile.ZipFile('/tmp/portfoliobuild.zip') as myzip:
@@ -17,8 +19,6 @@ def lambda_handler(event, context):
             portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
             portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
 
-    sns = boto3.resource('sns')
-    topic = sns.Topic('arn:aws:sns:us-east-1:390072403864:portfolio-topic')
     response = topic.publish(
     Message='Your Portfolio has been successfully updated',
     Subject='Portfolio update',
